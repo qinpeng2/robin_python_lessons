082d54573675e214474fdeedcc21acc8ee4e785f
allen.qin <allen.qin@yeahmobi.com>
2019-04-05 08:17:28 +0800

    lec09 -> aqi_v4.0 OS, with, read csv and json

---
+++ lec08/plt_burndown.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    week = []
+    plt.plot([1, 2, 3, 4], [4, 3, 2, 1], [1, 2, 3, 4], [1, 2])
+    plt.show()
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ lec09/aqi_v4.0.py
@@ -0,0 +1,48 @@
+"""
+    功能：空气质量指数
+    作者：Allen
+    版本：4.0
+    说明：v4.0 OS，with，json与csv文件的读取
+    日期：2019/04/05
+"""
+import os
+import json
+import csv
+
+
+def read_json_file(file_path):
+    """
+        解析json文件
+    """
+    with open(file_path, mode='r', encoding='utf-8') as f:
+        data_list = json.load(f)
+    print(data_list)
+
+
+def read_csv_file(file_path):
+    """
+        解析csv文件
+    """
+    with open(file_path, mode='r', encoding='utf-8', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(",".join(row))
+
+
+def main():
+    """
+        主函数
+    """
+    file_path = input("请输入文件名:")
+    file_name, file_ext = os.path.splitext(file_path)
+
+    if file_ext == '.json':
+        read_json_file(file_path)
+    elif file_ext == '.csv':
+        read_csv_file(file_path)
+    else:
+        print('不支持的文件格式')
+
+
+if __name__ == '__main__':
+    main()
