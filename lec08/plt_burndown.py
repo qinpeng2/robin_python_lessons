1d7cc66e2d27f4f63ce095ca56454cef61c719a9
allen.qin <allen.qin@yeahmobi.com>
2019-04-11 20:27:55 +0800

    burndown chart

--- lec08/plt_burndown.py
+++ lec08/plt_burndown.py
@@ -1,12 +1,33 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import pandas
+
+
+def read_personal_data():
+    week_data = pandas.read_csv('data.csv')
+    return week_data['km']
 
 
 def main():
-    week = []
-    plt.plot([1, 2, 3, 4], [4, 3, 2, 1], [1, 2, 3, 4], [1, 2])
+    target = 1000
+    weeks = 52
+    week_target = []
+    week_actual = []
+    # target list
+    for i in range(weeks):
+        week_target.append(target - target/weeks * i)
+    # actual list
+    personal_data = read_personal_data()
+
+    total_km = 0
+    for km in personal_data:
+        total_km += km
+        week_actual.append(target - total_km)
+
+    plt.title('Burn Down Chart')
+    plt.plot(week_target)
+    plt.plot(week_actual)
+    plt.plot()
     plt.show()
-    pass
 
 
 if __name__ == '__main__':
